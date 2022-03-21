7445d3f9b25f886023b99e8d10e587c8cebea3af
Matheus <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-03-21 05:39:08 -0300

    Integrando Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
-
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(router);
+
 
 app.listen(3333, () => console.log("Server is running"));
